1fd1a0accfcfe16f2cbef73e0514fb389f0b67d9
Prabal <prabal1.gupta@gmail.com>
2021-05-05 19:15:47 +0530

    Changes made in api

--- api/models/Weather.php
+++ api/models/Weather.php
@@ -8,28 +8,8 @@ class Weather {
     public function __construct($db) {
         $this->conn = $db;
     }
-
     
-    public function getWeather($apiUrl = null, $apiKey = null, $city = null) {
-        try {
-            $curl = curl_init();
-            curl_setopt_array($curl, [CURLOPT_URL => $apiUrl . urldecode($city), CURLOPT_RETURNTRANSFER => true, CURLOPT_FOLLOWLOCATION => true, CURLOPT_ENCODING => "", CURLOPT_MAXREDIRS => 10, CURLOPT_TIMEOUT => 30, CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1, CURLOPT_CUSTOMREQUEST => "GET", CURLOPT_HTTPHEADER => ["x-rapidapi-host: community-open-weather-map.p.rapidapi.com", "x-rapidapi-key: " . $apiKey, ], ]);
-            $response = curl_exec($curl);
-            $response = json_decode($response);
-            $temp = $response->main;
-            $err = curl_error($curl);
-            curl_close($curl);
-            if ($err) {
-                throw new Exception("cURL Error #:" . $err);
-            } else {
-                return $temp;
-            }
-        }
-        catch(\Exception $e) {
-            error_log($e->getMessage());
-            return $e->getMessage();
-        }
-    }
+  
     public function getLastWeather() {
         $query = "SELECT city,value,added_date FROM " . $this->table_name . " order by id DESC LIMIT 1";
         $stmt = $this->conn->prepare($query);
